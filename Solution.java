47d1a80760d205171db601fd846f03f47df15e9f
Wong <allison.wong@fiserv.com>
2023-10-31 14:42:58 -0400

    Solution to AH5

--- Solution.java
+++ Solution.java
@@ -1,12 +1,32 @@
+import java.util.Collections;
+import java.util.HashMap;
+
 class Solution {
     public static char mostFrequentChar(String s) {
-      
-      return;
+      char[] s_arr = s.toCharArray();
+      HashMap<Character, Integer> characterDict = new HashMap<>();
+      char max_char = s_arr[0];
+
+      for (char c : s_arr) {
+        if (characterDict.containsKey(c)) {
+          characterDict.put(c, characterDict.get(c) + 1);
+          if (characterDict.get(c) >= characterDict.get(max_char)){
+            max_char = c;
+          }
+        } else {
+          characterDict.put(c, 1);
+        }
+      }
+    
+      return max_char;
     }
   
     public static void main(String[] args) {
       // Try out your test cases:
       System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("david")); // -> 'd'
+      System.out.println(Solution.mostFrequentChar("mississippi")); // -> 'i'
+      System.out.println(Solution.mostFrequentChar("eleventennine")); // -> 'e'
     }
 
   }
